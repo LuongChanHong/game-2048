b4ce1476082f756177f7b11c6eae7c8e2c674c4f
honglc <hongmainmail@gmail.com>
2024-03-20 23:36:45 +0700

    add Home main layout element

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Main from "./pages/Main/Main";
+import Home from "./pages/Home/Home";
+import Chessboard from "./pages/Chessboard/Chessboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/chessboard" element={<Chessboard />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <section className="homeContainer container p-4 ">
+      <section className="row">
+        <section className="homeWrapper">
+          <section>Lever Chessbroad</section>
+          <section>Lever Selection</section>
+          <section>
+            <button>Game Start</button>
+          </section>
+        </section>
+      </section>
+    </section>
+  );
+};
+
+export default Home;
