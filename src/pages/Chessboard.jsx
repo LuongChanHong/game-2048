12c81d92bf7cf0d389976a37d078b189f9a31509
honglc <hongmainmail@gmail.com>
2024-03-28 23:22:36 +0700

    coding render chessboard squares func

--- src/pages/Chessboard.jsx
+++ src/pages/Chessboard.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/common.css";
 import "../css/color.css";
 import "../css/chessboard.css";
 
 const Chessboard = () => {
+  const level = 3;
+
+  const renderSquare = () => {
+    return <div className="square"></div>;
+  };
+
+  const renderChessboard = () => {
+    const squares = [];
+    for (let i = level; i < level; i++) {}
+
+    return <section className="container"></section>;
+  };
+
   return <section>Chessboard</section>;
 };
 
