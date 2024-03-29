2b9b7a63c74f0e484046dfa2d8ec64b6a7650613
honglc <hongmainmail@gmail.com>
2024-03-29 18:27:27 +0700

    converting js list to html

--- src/pages/Chessboard.jsx
+++ src/pages/Chessboard.jsx
@@ -4,20 +4,34 @@ import "../css/color.css";
 import "../css/chessboard.css";
 
 const Chessboard = () => {
+  const [square, setSquare] = useState([]);
   const level = 3;
+
+  const createSquareValue = (level) => {
+    let _squares = [];
+    for (let i = 0; i < level * level; i++) {
+      _squares.push(0);
+    }
+    return _squares;
+  };
 
   const renderSquare = () => {
     return <div className="square"></div>;
   };
 
   const renderChessboard = () => {
-    const squares = [];
-    for (let i = level; i < level; i++) {}
+    const _squares = createSquareValue(level);
 
-    return <section className="container"></section>;
+    for (let i = 0; i < _squares.length; i++) {}
+    return 0;
   };
+  renderChessboard();
 
-  return <section>Chessboard</section>;
+  return (
+    <section>
+      <section className="container">Chessboard</section>
+    </section>
+  );
 };
 
 export default Chessboard;
