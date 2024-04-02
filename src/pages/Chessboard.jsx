84a97bb5a3cc721743ea2a9edf379763bd56acc1
honglc <hongmainmail@gmail.com>
2024-04-02 22:37:39 +0700

    testing another render chessboard idea

--- src/pages/Chessboard.jsx
+++ src/pages/Chessboard.jsx
@@ -10,7 +10,7 @@ const Chessboard = () => {
   const createSquareValue = (level) => {
     let _squares = [];
     for (let i = 0; i < level * level; i++) {
-      _squares.push(0);
+      _squares.push(1);
     }
     return _squares;
   };
@@ -21,7 +21,10 @@ const Chessboard = () => {
 
   const renderChessboard = () => {
     const _squares = createSquareValue(level);
-    // let row = `<section className="row">`;
+    let row = <section className="row"></section>;
+
+    console.log(row);
+
     // for (let i = 0, j = 1; i < _squares.length; i++, j++) {
     //   if (j % level != 0) {
     //     row += `<div className="col">${i}</div>`;
@@ -29,19 +32,46 @@ const Chessboard = () => {
     //     row += `<div className="col">${i}</div></section><section className="row">`;
     //   }
     // }
-    let row = _squares.map((item, i) => (
-      <div className="boardSquare col-4">
-        <div className="boardSquare--content">{i}</div>
-      </div>
-    ));
+
+    // let row = _squares.map((item, i) => (
+    //   <div className="boardSquare">
+    //     <div className="boardSquare--content">{i}</div>
+    //   </div>
+    // ));
     // console.log(row);
-    return row;
+    return 0;
+  };
+  // renderChessboard();
+
+  const displayMatrix = (matrixSize) => {
+    const _squares = createSquareValue(level);
+    let tableHTML = "";
+
+    for (let i = 0; i < matrixSize * matrixSize; i++) {
+      const row = Math.floor(i / matrixSize);
+      const col = i % matrixSize;
+
+      // const cellValue = _squares[i] || ""; // Tránh trường hợp mảng không đủ phần tử
+      tableHTML += `<td>${_squares[i]}</td>`;
+
+      if ((i + 1) % matrixSize === 0) {
+        tableHTML += "</tr>";
+        if (i !== matrixSize * matrixSize - 1) {
+          tableHTML += "<tr>";
+        }
+      }
+    }
+
+    return tableHTML;
   };
 
   return (
     <section>
       <section className="container">
-        <div className="boardEdge row">{renderChessboard()}</div>
+        {/* <div className="boardEdge ">{displayMatrix(3)}</div> */}
+        <table>
+          <tbody dangerouslySetInnerHTML={{ __html: displayMatrix(3) }} />
+        </table>
       </section>
     </section>
   );
